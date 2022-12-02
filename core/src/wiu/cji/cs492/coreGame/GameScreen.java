75834530df416dc2763690a184b1ae5f2bc4a279
Joytaylor <jtaylor2@imsa.edu>
2022-12-01 22:54:11 -0600

    newDeathSound

--- core/src/wiu/cji/cs492/coreGame/GameScreen.java
+++ core/src/wiu/cji/cs492/coreGame/GameScreen.java
@@ -69,7 +69,7 @@ public class GameScreen implements Screen {
         //Calls to helper class
         this.orthogonalTiledMapRenderer = tileMapHelper.setUpMap(); //Can we use this to pass levels?
 
-        FaintSound = Gdx.audio.newMusic(Gdx.files.internal("SoundEffects/FaintSound.mp3"));
+        FaintSound = Gdx.audio.newMusic(Gdx.files.internal("SoundEffects/FaintSound1.mp3"));
 
         //Camera
         gamecam = new OrthographicCamera();
